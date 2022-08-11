5a29ff0d37864ad67d8061c5a0f8b1f60c43d786
HongWei <57443207+Night-Star04@users.noreply.github.com>
2022-08-12 00:48:30 +0800

    Mongo Load and Connect implement

---
+++ src/client.ts
@@ -0,0 +1,111 @@
+import mongoose from "mongoose";
+import fs from "fs";
+import type { Models, ConnectOptions } from "mongoose";
+
+export class mongoCilent {
+	public Monogoose = mongoose;
+	public Data: Map<string, Models>;
+
+	private Connectton: boolean;
+	private ConnectNumber: number;
+	private MongoUrl: string;
+	private cooldown: Map<string, number>;
+	private CDdalay: number;
+	private ModlePath: string;
+
+	constructor(MongoUrl: string, ModlePath: string, dalay?: number) {
+		this.Data = new Map();
+		this.cooldown = new Map();
+		this.Connectton = false;
+		this.ConnectNumber = 0;
+		this.CDdalay = dalay || 30;
+		this.MongoUrl = MongoUrl;
+		this.ModlePath = ModlePath;
+		this.load();
+	}
+
+	private async load() {
+		if (!fs.statSync(this.ModlePath).isDirectory())
+			throw new Error("ModlePath is not a directory");
+		const files = fs
+			.readdirSync(this.ModlePath)
+			.filter((file) => file.endsWith(".ts") || file.endsWith(".js"));
+
+		for (const file of files) {
+			const model = require(`${this.ModlePath}\\${file}`);
+			const { Name, Models } = model;
+			if (typeof Name !== "string")
+				throw new Error("ModleName is not a string");
+
+			this.Data.set(Name, Models);
+		}
+	}
+
+	public async Connect(options?: ConnectOptions) {
+		if (this.ConnectNumber === 0) {
+			await this.Monogoose.connect(this.MongoUrl, options)
+				.then((mongoose) => {
+					this.Connectton = true;
+
+					// start listening
+					const db = mongoose.connection;
+					db.on("connecting", () => console.log("connecting to MongoDB..."));
+					db.on("connected", () => console.log("connected to MongoDB!"));
+					db.on("disconnected", async () => {
+						console.log("disconnected from MongoDB!");
+						this.Connectton = false;
+						await this.Connect(options);
+					});
+					db.on("reconnected", () => {
+						console.log("reconnected to MongoDB!");
+						this.Connectton = true;
+					});
+					db.on("open", () => console.log("Connection to MongoDB"));
+					db.on("error", async (error) => {
+						console.error(error);
+						this.Connectton = false;
+						await this.Connect(options);
+					});
+				})
+				.catch((err: any) => {
+					this.Connectton = false;
+					throw err;
+				});
+			console.log("Connected");
+		} else if (this.ConnectNumber > 0) {
+			const now = new Date().getTime();
+			const amount = this.CDdalay * 1000;
+			const last: number | undefined = this.cooldown.get("mongodb");
+
+			if (last) {
+				const expirationTime = last + amount;
+
+				if (now < expirationTime) {
+					const timeLeft = (expirationTime - now) / 1000;
+
+					console.log(`Reconnecting in ${timeLeft} seconds`);
+					this.ConnectNumber++;
+					return;
+				}
+			}
+
+			if (!this.Connectton) {
+				await this.Monogoose.connect(this.MongoUrl, options)
+					.then(() => {
+						this.Connectton = true;
+					})
+					.catch((err) => {
+						this.Connectton = false;
+						throw err;
+					});
+			}
+
+			this.cooldown.set("mongo", now);
+
+			setTimeout(async () => {
+				this.cooldown.delete("mongo");
+			}, amount);
+		}
+		this.ConnectNumber += 1;
+	}
+}
